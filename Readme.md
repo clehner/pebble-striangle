9f1ab093c23cccd877469fee95045ad15f414965
Charles Lehner <cel@celehner.com>
2015-04-05 08:36:05 -0400

    Allow adding multiple triangles

--- Readme.md
+++ Readme.md
@@ -3,3 +3,9 @@
 ![screenshot](pebble-striangle-screenshot.png)
 
 Attempts at 1D cellular automata on a Pebble
+
+## Keys
+
+- up: reset
+- down: scroll down
+- select: toggle a triangle at current location
--- src/pebble-striangle.c
+++ src/pebble-striangle.c
@@ -4,36 +4,57 @@
 
 static Window *window;
 static Layer *window_layer;
-static uint8_t factor = 0b11;
+static uint8_t seed[WIDTH/8] = {0},
+               last[WIDTH/8] = {0};
 
-static void multiply(uint8_t *from, uint8_t *to, uint8_t factor, uint8_t len) {
-  uint16_t carry = 0;
-  for (int i = 0; i < len; i++) {
-    uint16_t val = from[i] * factor;
-    to[i] = val | carry;
-    carry = val >> 8;
+static void iterate(uint8_t *from, uint8_t *to, uint8_t len) {
+  uint8_t carry = 0;
+  for (int x = 0; x < len; x++) {
+    uint16_t value = carry ^ from[x] ^ (from[x] << 1);
+    to[x] = value;
+    carry = value >> 8;
   }
 }
 
+static void reset_seed() {
+  for (int i = 0; i < WIDTH/8; i++)
+    seed[i] = 0;
+  seed[0] = 1;
+}
+
+static void copy_row(uint8_t *from, uint8_t *to) {
+  for (int i = 0; i < WIDTH / 8; i++)
+    to[i] = from[i];
+}
+
+static void select_long_click_handler(ClickRecognizerRef recognizer,
+    void *context) {
+  reset_seed();
+  layer_mark_dirty(window_layer);
+}
+
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  factor = 1;
+  seed[0] ^= 1;
   layer_mark_dirty(window_layer);
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  factor++;
+  reset_seed();
   layer_mark_dirty(window_layer);
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  factor--;
+  copy_row(seed, last);
+  iterate(last, seed, WIDTH / 8);
   layer_mark_dirty(window_layer);
 }
 
 static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_repeating_click_subscribe(BUTTON_ID_UP, 50, up_click_handler);
-  window_single_repeating_click_subscribe(BUTTON_ID_DOWN, 50,
+  window_long_click_subscribe(BUTTON_ID_SELECT, 500,
+      select_long_click_handler, NULL);
+  window_single_repeating_click_subscribe(BUTTON_ID_UP, 30, up_click_handler);
+  window_single_repeating_click_subscribe(BUTTON_ID_DOWN, 30,
       down_click_handler);
 }
 
@@ -46,15 +67,13 @@ static void update_proc(Layer *this_layer, GContext *ctx) {
 
   // assume buffer->bounds is the whole layer
 
-  // clear first row except for one pixel
-  for (int j = 0; j < WIDTH / 8; j++)
-    data[j] = 0;
-  data[4] = 1;
+  // set first row to seed
+  for (int i = 0; i < row_size; i++)
+    data[i] = seed[i];
 
+  // update the rest of the rows
   for (int y = 1; y < bounds.size.h; y++) {
-      uint8_t *row_curr = &data[row_size * y],
-              *row_prev = &data[row_size * (y-1)];
-      multiply(row_prev, row_curr, factor, WIDTH / 8);
+    iterate(&data[row_size * (y-1)], &data[row_size * y], row_size);
   }
 
   graphics_release_frame_buffer(ctx, buffer);
@@ -64,19 +83,14 @@ static void window_load(Window *window) {
   window_layer = window_get_root_layer(window);
 
   layer_set_update_proc(window_layer, update_proc);
-  /*
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
-  */
 }
 
 static void window_unload(Window *window) {
-  // text_layer_destroy(text_layer);
 }
 
 static void init(void) {
+  reset_seed();
+
   window = window_create();
   window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
